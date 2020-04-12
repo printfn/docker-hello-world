ba22c6d9d85f98d484c0289fe4071b4f7de6b48e
printfn <printfn@users.noreply.github.com>
2020-04-12 18:08:05 +0200

    Remove extra newline, size = 13360

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -7,7 +7,7 @@ extern crate libc;
 #[no_mangle]
 pub extern "C" fn main(_argc: isize, _argv: *const *const u8) -> isize {
     // Since we are passing a C string the final null character is mandatory.
-    const HELLO: &'static str = "Hello, world!\n\0";
+    const HELLO: &'static str = "Hello, world!\0";
     unsafe {
         libc::puts(HELLO.as_ptr() as *const _);
     }
