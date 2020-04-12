2be7bf589988cb0a23a140160d8d0b9d3ef23cc0
printfn <printfn@users.noreply.github.com>
2020-04-13 00:24:00 +0200

    Add 04-asm step

---
+++ 04-asm/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+
+nasm -f bin -o a.out hello-world.asm
+chmod +x a.out
+wc -c a.out
+stat --format="Binary size: %s" a.out
+docker build --tag printfn/hello-world .
+docker image inspect printfn/hello-world:latest -f "Image size: {{ json .Size }}"
+docker run --rm printfn/hello-world
+
