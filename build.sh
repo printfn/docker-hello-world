94b8e55b77d58fc52589fede2e9a61e22ab78797
printfn <printfn@users.noreply.github.com>
2020-04-12 18:02:18 +0200

    Reduce binary size to 13360, image size is 26664

--- build.sh
+++ build.sh
@@ -5,5 +5,8 @@ set -euo pipefail
 cd hello-world
 cargo +nightly build --target x86_64-unknown-linux-musl --release
 cd ..
+strip hello-world/target/x86_64-unknown-linux-musl/release/hello-world
 docker build --tag printfn/hello-world .
+stat --format="Binary size: %s" hello-world/target/x86_64-unknown-linux-musl/release/hello-world
+docker image inspect printfn/hello-world:latest -f "Image size: {{ json .Size }}"
 docker run --rm printfn/hello-world
--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -9,7 +9,7 @@ pub extern "C" fn main(_argc: isize, _argv: *const *const u8) -> isize {
     // Since we are passing a C string the final null character is mandatory.
     const HELLO: &'static str = "Hello, world!\n\0";
     unsafe {
-        libc::printf(HELLO.as_ptr() as *const _);
+        libc::puts(HELLO.as_ptr() as *const _);
     }
     0
 }
