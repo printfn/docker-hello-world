39660b4851471d4a8c5e42bfdf4c41842ff15b88
printfn <printfn@users.noreply.github.com>
2020-04-13 04:46:50 +0200

    Clean up code

--- 04-asm/hello-world.asm
+++ 04-asm/hello-world.asm
@@ -2,53 +2,60 @@
 bits 64
         org 0x00010000
 
-ehdr:   db 0x7f, "ELF"   ; e_ident: magic
-        db 2             ; 64-bit executable
-
-_start: xor eax, eax     ; automatically zeroes upper 32 bits
-        inc eax          ; could also inc al, no difference
+ehdr:   db 0x7f, "ELF"      ; e_ident
+        db 2
+_start: xor eax, eax
+        inc eax
         mov esi, msg
         jmp cont
-
-;        db 1             ;          little-endian                         
-;        db 0             ;          hp-ux abi
-;        db 0             ;          version "0"
-;        db 0,0,0,
-          ;       padding             
-        db 2,0           ; e_type: executable
-        db 0x3e,0        ; e_machine (must be 0x3e == 62 for amd64)
-        ;db 0,0,0,0       ; e_version (free to use for anything)
-cont2:  dec edi
-        syscall   ; exit: rax = 60, rdi = 0
-        dq _start  ; e_entry (entry point address)
-        db 56,0,0,0, 0,0,0,0 ; e_phoff (program header offset) (IMPORTANT: = phdr - ehdr)
-
-cont:   mov edi, eax
+        dw 2                ; e_type
+        dw 62               ; e_machine
+cont2:  dec edi             ; e_version
+        syscall  ; exit
+        dq _start           ; e_entry
+        dq (phdr - ehdr)    ; e_phoff
+cont:   mov edi, eax        ; e_shoff
         xor edx, edx
         mov dl, 12
-        syscall          ; write: rax = rdi = 1, rsi = msg, rdx = 13
-        xor eax, eax
+        syscall  ; write
+        xor eax, eax        ; e_flags
         mov al, 60
-        jmp cont2
-          ;db 0,0,0,0, 0,0,0,0  ; e_shoff (section header offset)
-                     ; e_flags (processor-specific flags)
-        ;db 0, 0      ; e_ehsize (elf header size)
-        db 56,0      ; e_phentsize (program header size) (always equals 56, checked by linux)
-phdr:   db 1, 0          ; e_phnum (number of program header entries)
-        db 0, 0          ; e_shentsize (size of section header entry)
-        db 1, 0          ; e_shnum (number of section header entries)
-        db 0, 0          ; e_shstrndx (section name string table index)
+        jmp cont2           ; e_ehsize
+        dw 56               ; e_phentsize
+phdr:   dw 1                ; e_phnum          p_type
+        dw 0                ; e_shentsize
+        dw 1                ; e_shnum          p_flags
+        dw 0                ; e_shstrndx
+        dq 0                ;                  p_offset
+        dq ehdr             ;                  p_vaddr
+msg:    db 'Hello Wo'       ;                  p_paddr
+        db 'rld',10,0,0,0,0 ;                  p_filesz
+        db 'rld',10,0,0,0,0 ;                  p_memsz
+        dq 1                ;                  p_align
 
-; END OF EHDR
+; e_ident must start with 0x7f, "ELF", 2, followed by 11 arbitrary bytes
+; e_type must be 2 (64-bit executable)
+; e_machine must be 62 (amd64)
+; e_version is arbitrary
+; e_entry is the program entrypoint address
+; e_phoff is program header offset
+; e_shoff: section header offset (arbitrary)
+; e_flags: processor-specific flags (arbitrary)
+; e_ehsize: elf header size (arbitrary)
+; e_phentsize: program header size: must be 56
+; e_phnum: number of program header entries
+; e_shentsize: size of section header entry
+; e_shnum: number of section header entries
+; e_shstrndx: section name string table index
 
-        ; p_type (loadable segment)
-        ;db 5, 0, 0, 0   ; p_flags (segment attributes, 0x1: exec, 0x2: write, 0x4: read)
-        db 0,0,0,0, 0,0,0,0    ; p_offset (offset in file)
-        dq ehdr    ; p_vaddr (virtual address in memory)
+; p_type: type of segment, must be 1 for loadable segment
+; p_flags: segment attributes, 0x1 exec, 0x2 write, 0x4 read, 0x1 and 0x4 each imply the other
+; p_offset: offset in file, must be 0
+; p_vaddr: virtual address in memory
+; p_paddr: reserved (arbitrary)
+; p_filesz: size of segment in file
+; p_memsz: size of segment in memory
+; p_align: alignment of segment
 
-msg:    db 'Hello Wo'          ; p_paddr (reserved)
-        db 'rld',10,1,0,0,0      ; p_filesz (size of segment in file)
-        db 'rld',10,1,0,0,0      ; p_memsz (size of segment in memory)
-        db 1,0,0,0, 0,0,0,0        ; p_align (alignment of segment)
-
-; END OF PHDR
+; write syscall: rax = rdi = 1, rsi = msg, rdx = 12
+; exit syscall: rax = 60, rdi = 0
