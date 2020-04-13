15bb4d08f874f67c759d0a8a3338d6962d4a7756
printfn <printfn@users.noreply.github.com>
2020-04-13 03:44:08 +0200

    Reduce to 116 bytes

--- 04-asm/hello-world.asm
+++ 04-asm/hello-world.asm
@@ -1,59 +1,55 @@
 ; hello-world.asm
 bits 64
-        org 0x08048000
+        org 0x02000000
 
-; $:  beginning of the current line
-; $$: beginning of the current section
+ehdr:   db 0x7f, "ELF"   ; e_ident: magic
+        db 2             ; 64-bit executable
 
-ehdr:
-	db 0x7f, "ELF"   ; e_ident: magic
-        db 2             ;          64-bit
-        db 1             ;          little-endian
-        db 1             ;          hp-ux abi
-        db 0             ;          version "0"
-times 8 db 0             ;          padding
-	dw 2             ; e_type: executable
-        dw 0x3e          ; e_machine (must be 0x3e == 62 for amd64)
-        dd 1             ; e_version
-        dq _start        ; e_entry (entry point address)
-        dq (phdr - ehdr) ; e_phoff (program header offset)
-        dq 0             ; e_shoff (section header offset)
-        dd 0             ; e_flags (processor-specific flags)
-        dw ehdrsize      ; e_ehsize (elf header size)
-        dw phdrsize      ; e_phentsize (program header size)
-        db 1, 0          ; e_phnum (number of program header entries)
-        db 0, 0          ; e_shentsize (size of section header entry)
-        db 0, 0          ; e_shnum (number of section header entries)
-        db 0, 0          ; e_shstrndx (section name string table index)
-
-ehdrsize equ $ - ehdr    ; 64 bytes
-
-phdr:
-        db 1, 0, 0, 0    ; p_type (loadable segment)
-        db 5, 0, 0, 0    ; p_flags (segment attributes, 0x1: exec, 0x2: write, 0x4: read)
-        dq 0             ; p_offset (offset in file)
-        dq $$            ; p_vaddr (virtual address in memory)
-        dq phdr          ; p_paddr (reserved)
-        dq filesize      ; p_filesz (size of segment in file)
-        dq filesize      ; p_memsz (size of segment in memory)
-        dq 0x1000        ; p_align (alignment of segment)
-
-phdrsize equ $ - phdr    ; 56 bytes
-
-; 25 bytes of code, followed by 12 bytes of data
-_start:
-	xor eax, eax     ; automatically zeroes upper 32 bits
-	inc eax          ; could also inc al, no difference
-	mov edi, eax
+        xor eax, eax     ; automatically zeroes upper 32 bits
+        inc eax          ; could also inc al, no difference
         mov esi, msg
-	xor edx, edx
+        jmp cont2
+
+;        db 1             ;          little-endian                         
+;        db 0             ;          hp-ux abi
+;        db 0             ;          version "0"
+;        db 0,0,0,
+          ;       padding             
+        db 2,0           ; e_type: executable
+        db 0x3e,0        ; e_machine (must be 0x3e == 62 for amd64)
+        ;db 0,0,0,0       ; e_version (free to use for anything)
+cont:   dec edi
+        syscall
+        db 5,0,0,2, 0,0,0,0  ; e_entry (entry point address)
+        db 60,0,0,0, 0,0,0,0 ; e_phoff (program header offset) (IMPORTANT: = phdr - ehdr)
+
+cont2:  mov edi, eax
+        xor edx, edx
         mov dl, 12
         syscall          ; write: rax = rdi = 1, rsi = msg, rdx = 13
         xor eax, eax
         mov al, 60
-        xor edi, edi
-        syscall          ; exit: rax = 60, rdi = 0
-msg:    db  "Hello World", 10
+        jmp cont
+          ;db 0,0,0,0, 0,0,0,0  ; e_shoff (section header offset)
+                     ; e_flags (processor-specific flags)
+        ;db 0, 0      ; e_ehsize (elf header size)
+        db 56,0      ; e_phentsize (program header size) (always equals 56, checked by linux)
+        db 1, 0          ; e_phnum (number of program header entries)
+        db 0, 0          ; e_shentsize (size of section header entry)
+phdr:   db 1, 0          ; e_shnum (number of section header entries)
+        db 0, 0          ; e_shstrndx (section name string table index)
 
-filesize equ $ - $$      ; 64 + 56 + 25 + 12 = 157 bytes
+        ; p_type (loadable segment)
+        db 5, 0, 0, 0   ; p_flags (segment attributes, 0x1: exec, 0x2: write, 0x4: read)
+        db 0,0,0,0, 0,0,0,0    ; p_offset (offset in file)
+        db 0,0,0,2, 0,0,0,0    ; p_vaddr (virtual address in memory)
 
+msg:    db 'Hello Wo'          ; p_paddr (reserved)
+        db 'rld',10,0,0,0,0      ; p_filesz (size of segment in file)
+        db 'rld',10,0,0,0,0      ; p_memsz (size of segment in memory)
+        dq 0x1000        ; p_align (alignment of segment)
+
+; 25 bytes of code, followed by 12 bytes of data
+;cont:  dec edi
+;        syscall          ; exit: rax = 60, rdi = 0
+;msg:    db  "Hello World", 10
