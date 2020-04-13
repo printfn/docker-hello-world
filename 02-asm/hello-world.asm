1a3bc4424297384efa34c1b93944d353f69878b4
printfn <printfn@users.noreply.github.com>
2020-04-13 11:35:13 +0200

    Write string length in write syscall

--- 02-asm/hello-world.asm
+++ 02-asm/hello-world.asm
@@ -9,7 +9,7 @@ _start:
 	xor rdi, rdi
 	inc rdi       ; write to file descriptor 1 (stdout)
         mov rsi, msg  ; addr of message
-        mov rdx, 13   ; length of message
+        mov rdx, 12   ; length of message
         syscall       ; perform syscall
         mov rax, 60   ; code for 'exit' syscall
         xor dil, dil  ; exit code 0
