9badd1206582b2ab1982c0a5b496e3e4a6bcf891
printfn <printfn@users.noreply.github.com>
2020-04-13 10:12:36 +0200

    Improve file syntax

--- 04-asm/hello-world.asm
+++ 04-asm/hello-world.asm
@@ -2,36 +2,36 @@
 bits 64
         org 0x00010000
 
-ehdr:   db 0x7f, "ELF"      ; e_ident
-        db 2
-_start: xor eax, eax
-        inc eax
-        mov esi, msg
-        jmp cont
-        dw 2                ; e_type
-        dw 62               ; e_machine
-cont2:  dec edi             ; e_version
-        syscall  ; exit
-        dq _start           ; e_entry
-        dq (phdr - ehdr)    ; e_phoff
-cont:   mov edi, eax        ; e_shoff
-        xor edx, edx
-        mov dl, 12
-        syscall  ; write
-        xor eax, eax        ; e_flags
-        mov al, 60
-        jmp cont2           ; e_ehsize
-        dw 56               ; e_phentsize
-phdr:   dw 1                ; e_phnum          p_type
-        dw 0                ; e_shentsize
-        dw 1                ; e_shnum          p_flags
-        dw 0                ; e_shstrndx
-        dq 0                ;                  p_offset
-        dq ehdr             ;                  p_vaddr
-msg:    db 'Hello Wo'       ;                  p_paddr
-        db 'rld',10,0,0,0,0 ;                  p_filesz
-        db 'rld',10,0,0,0,0 ;                  p_memsz
-        dq 1                ;                  p_align
+ehdr:   db 0x7f, "ELF"      ; 7f 45 4c 46              e_ident
+        db 2                ; 02                         64-bit
+_start: xor eax, eax        ; 31 c0                      little-endian, hp-ux abi (both unused)
+        inc eax             ; ff c0                      version (unused), padding
+        mov esi, msg        ; be 50 00 01 00             5 bytes of padding
+        jmp cont            ; eb 18                      2 bytes of padding
+        dw 2                ; 02 00                    e_type
+        dw 62               ; 3e 00                    e_machine
+cont2:  dec edi             ; ff cf                    e_version
+        syscall             ; 0f 05 exit
+        dq _start           ; 05 00 01 00 00 00 00 00  e_entry
+        dq (phdr - ehdr)    ; 38 00 00 00 00 00 00 00  e_phoff
+cont:   mov edi, eax        ; 89 c7                    e_shoff
+        xor edx, edx        ; 31 d2
+        mov dl, 12          ; b2 0c
+        syscall             ; 0f 05 write
+        xor eax, eax        ; 31 c0                    e_flags
+        mov al, 60          ; b0 3c
+        jmp cont2           ; eb de                    e_ehsize
+        dw 56               ; 38 00                    e_phentsize
+phdr:   dw 1                ; 01 00                    e_phnum              p_type
+        dw 0                ; 00 00                    e_shentsize
+        dw 1                ; 01 00                    e_shnum              p_flags
+        dw 0                ; 00 00                    e_shstrndx
+        dq 0                ; 00 00                                         p_offset
+        dq ehdr             ; 00 00 01 00 00 00 00 00                       p_vaddr
+msg:    db 'Hello Wo'       ; 48 65 6c 6c 6f 20 57 6f                       p_paddr
+        db 'rld',10,0,0,0,0 ; 72 6c 64 0a 00 00 00 00                       p_filesz
+        db 'rld',10,0,0,0,0 ; 72 6c 64 0a 00 00 00 00                       p_memsz
+        dq 1                ; 01 00 00 00 00 00 00 00                       p_align
 
 ; e_ident must start with 0x7f, "ELF", 2, followed by 11 arbitrary bytes
 ; e_type must be 2 (64-bit executable)
